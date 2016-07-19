1f02ffb34511219c4091c22a5dedd16a0c690e06
Natalie Chow <cychownat@gmail.com>
2016-07-19 15:36:33 -0700

    Cleaned up code and restructured unit test

--- tests/rules/no-global-shortcuts.js
+++ tests/rules/no-global-shortcuts.js
@@ -3,33 +3,33 @@
 var rule = require(__dirname + '/../../lib/rules/no-global-shortcuts');
 var RuleTester = require('eslint').RuleTester;
 
-var ruleTester = new RuleTester();
+RuleTester.describe('/no-global-shortcuts', function() {
+  var ruleTester = new RuleTester();
 
-// Default options (disallow all methods)
-ruleTester.run('/no-global-shortcuts', rule, {
-  valid: [
-    'jasmine.getFixtures().set(\'<div></div>\');'
-  ],
-  invalid: [
-    {
-      code: 'setFixtures(\'<div></div>\');',
-      errors: [{ message: 'Unexpected global shortcut method setFixtures' }]
-    }
-  ]
-});
+  ruleTester.run('Default options (disallow all methods)', rule, {
+    valid: [
+      'jasmine.getFixtures().set(\'<div></div>\');'
+    ],
+    invalid: [
+      {
+        code: 'setFixtures(\'<div></div>\');',
+        errors: [{ message: 'Unexpected global shortcut method setFixtures' }]
+      }
+    ]
+  });
 
-// Customized options (allow specified methods)
-ruleTester.run('/no-global-shortcuts', rule, {
-  valid: [
-    {
-      code: 'setFixtures(\'<div></div>\');',
-      options: [['setFixtures']]
-    }
-  ],
-  invalid: [
-    {
-      code: 'loadFixtures(\'<div></div>\');',
-      errors: [{ message: 'Unexpected global shortcut method loadFixtures' }]
-    }
-  ]
-});
+  ruleTester.run('Customized options (allow specified methods)', rule, {
+    valid: [
+      {
+        code: 'setFixtures(\'<div></div>\');',
+        options: [['setFixtures']]
+      }
+    ],
+    invalid: [
+      {
+        code: 'loadFixtures(\'<div></div>\');',
+        errors: [{ message: 'Unexpected global shortcut method loadFixtures' }]
+      }
+    ]
+  });
+})
